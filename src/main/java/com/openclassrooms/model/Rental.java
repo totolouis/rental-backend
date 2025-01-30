8f52f2430b182975e5403b3e928871c4dbb413a8
totolouis <louismagand@gmail.com>
2025-01-30 14:50:43 -0800

    wip

---
+++ src/main/java/com/openclassrooms/model/Rental.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private Double surface;
+    private Double price;
+    private String picture;
+    private String description;
+    
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+    
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/openclassrooms/repository/RentalRepository.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.openclassrooms.model.Rental;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+    Optional<Rental> findById(String id);
+}
