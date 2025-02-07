4350b4404aee2e7ebfafeaa12c66c5dc8c9ab41f
Louis Magand Castel <louis.magand-castel@mastel.fr>
2025-02-07 19:46:00 +0100

    user register done

--- src/main/java/com/openclassrooms/controller/AuthController.java
+++ src/main/java/com/openclassrooms/controller/AuthController.java
@@ -2,23 +2,27 @@ package com.openclassrooms.controller;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.jaas.LoginExceptionResolver;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.openclassrooms.configuration.CustomUserDetails;
+import com.openclassrooms.model.User;
+import com.openclassrooms.repository.UserRepository;
 import com.openclassrooms.service.CustomUserDetailsService;
 import com.openclassrooms.service.JWTService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -28,15 +32,20 @@ public class AuthController {
 	private final CustomUserDetailsService userService; // private final AuthenticationManager authenticationManager;
 	// private final CustomUserDetailsService customUserDetailsService;
 	// private final JwtUtil jwtUtil;
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
 	
-	public AuthController(JWTService jwtService, CustomUserDetailsService userService) {
+	public AuthController(JWTService jwtService, CustomUserDetailsService userService, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
 		this.jwtService = jwtService;
 		this.userService = userService;
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 		// this.authenticationManager = authenticationManager;
 		// this.customUserDetailsService = customUserDetailsService;
 	}
 
-	// Define a DTO (Data Transfer Object) to represent the login request
+	// Define a DTO (Data Transfer Object) to represent the login request 
+	// TODO: put it somewhere else
 	public static class LoginRequest {
 		private String email;
 		private String password;
@@ -71,6 +80,18 @@ public class AuthController {
 		}
 	}
 
+	public static class RegisterRequest extends LoginRequest {
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name){
+			this.name = name;
+		}
+
+	}
 	// TODO: better handling of the AuthSuccess dto. A Dto is not neccesary and the return is dirty
 	// ! Redo
 	@PostMapping("/login")
@@ -103,18 +124,17 @@ public class AuthController {
 		return ResponseEntity.ok(userInfo);
 	}
 
-	// @PostMapping("/login2")
-	// public ResponseEntity<String> authenticateUser(@RequestBody LoginRequest
-	// loginRequest) {
-	// Authentication authentication = authenticationManager.authenticate(
-	// new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),
-	// loginRequest.getPassword())
-	// );
+	@PostMapping("/register")
+	public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
+		if (this.userRepository.findByEmail(registerRequest.getEmail()).isPresent()) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists.");
+		}
+		String encryptedPassword = this.passwordEncoder.encode(registerRequest.getPassword());
+		User newUser = new User(null, registerRequest.getEmail(), registerRequest.getName(), encryptedPassword, null,
+				null);
+		userRepository.save(newUser);
 
-	// String token = jwtService.generateToken(loginRequest.getEmail(),
-	// loginRequest.getPassword());
-
-	// return ResponseEntity.ok(jwt);
-	// }
-
+		return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully.");
+	}
+	
 }
